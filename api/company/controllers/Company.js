2bf9480f77f08cee6dbf74b8abd7a8d8fbbb4283
João Neto <joaoneto@baratocoletivo.com.br>
2019-12-12 10:47:29 -0300

    refactor: updated Jobs and Company relationship to be more easy to implement queries

--- api/company/controllers/Company.js
+++ api/company/controllers/Company.js
@@ -4,4 +4,23 @@
  * Read the documentation () to implement custom controller functions
  */
 
-module.exports = {};
+module.exports = {
+    async countJobs() {
+        const qtd =1;
+        let itens = [];
+        let cnt   = 0;
+        itens = await strapi.query('company').model.find().then( data => {
+            const e = strapi.query('job').model.where( { 'company': data }).countDocuments( ).fetch();
+            console.log( "e", e );
+            itens.push({
+                company: data,
+                jobsCount: cnt
+            });
+
+            return itens;
+        });
+
+        console.log( "hello", itens );
+        return itens;
+    }
+};
